9cd91ea97833d3c2ed17f1a27bb84183b8d606de
Helder <he7d3r@gmail.com>
2014-08-18 18:11:01 -0300

    Inform the license: CC BY-SA 3.0

--- src/ButtonsForRefs.js
+++ src/ButtonsForRefs.js
@@ -1,6 +1,7 @@
 /**
  * Add buttons to editor toolbar for some common references I use
- * @author: [[User:Helder.wiki]]
+ * @author: Helder (https://github.com/he7d3r)
+ * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/ButtonsForRefs.js]] ([[File:User:Helder.wiki/Tools/ButtonsForRefs.js]])
  */
 /*jslint browser: true, white: true*/
