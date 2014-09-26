a8451d9283058d23e514a4e9a29558277437791f
Helder <he7d3r@gmail.com>
2014-09-26 15:21:23 -0300

    Define JSHint configuration on .jshintrc

--- src/ButtonsForRefs.js
+++ src/ButtonsForRefs.js
@@ -3,8 +3,6 @@
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
-/*jslint browser: true, white: true*/
-/*global mediaWiki, jQuery */
 ( function ( mw , $ ) {
 'use strict';
 
@@ -228,6 +226,7 @@ if ( $.inArray(mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ) {
 				$.ready
 			).then( customizeToolbar );
 		}
+		/*jshint eqeqeq:true*/
 	} );
 }
 
